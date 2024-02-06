98f5acd6b3cefeaab33eb24a5aa5f3db44783e82
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-02-05 22:30:50 -0300

    FEAT : add search input & favicon.

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,7 +1,17 @@
+import logo from './assets/logo-nlw-expert.svg';
+
 export default function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <main className='mx-auto max-w-6xl my-12 space-y-6'>
+      <img src={logo} alt="NLW Expert" />
+
+      <form className='w-full'>
+        <input 
+          type="text" 
+          placeholder='Busque em suas notas...' 
+          className='w-full bg-transparent text-3xl font-semibold outline-none tracking-tight place-holder:text-slate-500'
+        />
+      </form>
+    </main>
   )
 }
