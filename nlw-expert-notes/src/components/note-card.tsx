a8a01b0bcf2b38a529045927873188106d86a067
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-02-06 22:17:24 -0300

    STYLE : add focus on cards.

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -1,6 +1,6 @@
 export default function NoteCard() {
     return (
-        <article className="rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative">
+        <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
             <span className="text-sm font-medium text-slate-300">
                 Há 2 dias
             </span>
@@ -10,6 +10,6 @@ export default function NoteCard() {
             </p>
 
             <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </article>
+        </button>
     )
 }
