a9fbc7fd58cc0e71b92a8a26417680986bc4b4c2
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-02-07 21:42:59 -0300

    FEAT : add modal.

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from '@radix-ui/react-dialog'
+
 interface NoteCardProps {
     note: {
         date: Date
@@ -7,15 +9,23 @@ interface NoteCardProps {
 
 export default function NoteCard({ note }: NoteCardProps) {
     return (
-        <button className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
-            <span className="text-sm font-medium text-slate-300">
-                {note.date.toISOString()}
-            </span>
-            <p className="text-sm leading-6 text-slate-400">
-                {note.content}
-            </p>
+        <Dialog.Root>
+            <Dialog.Trigger className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
+                <span className="text-sm font-medium text-slate-300">
+                    {note.date.toISOString()}
+                </span>
+                <p className="text-sm leading-6 text-slate-400">
+                    {note.content}
+                </p>
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+            </Dialog.Trigger>
 
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </button>
+            <Dialog.Portal>
+                <Dialog.Overlay className="fixed inset-0 bg-black/60" />
+                <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+                    Oi
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     )
 }
