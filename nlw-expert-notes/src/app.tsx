be6286e19e9e246d2ee37e4c06cc92a6e8b046fd
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-02-10 11:49:54 -0300

    FEAT : add responsive layout.

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -46,7 +46,7 @@ export default function App() {
     : notes;
 
   return (
-    <main className='mx-auto max-w-6xl my-12 space-y-6'>
+    <main className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
       <img src={logo} alt="NLW Expert" />
 
       <form className='w-full'>
@@ -60,7 +60,7 @@ export default function App() {
 
       <div className='h-px bg-slate-700' />
 
-      <section className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onNoteCreated={onNoteCreated} />
         
         {filteredNotes.map(note => {
