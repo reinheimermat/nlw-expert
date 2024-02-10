1a21139870dab3af73ef07ac48f0300df9ccc768
Matheus Reinheimer <mreinheimer0@gmail.com>
2024-02-10 00:05:37 -0300

    FEAT : add note renderization.

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,13 +1,13 @@
+import { useState } from 'react';
 import logo from './assets/logo-nlw-expert.svg';
 import NewNoteCard from './components/new-note-card';
 import NoteCard from './components/note-card';
 
-const note = {
-  date: new Date(),
-  content: 'hello world'
-}
-
 export default function App() {
+  const [notes, setNotes] = useState([
+    { id: 1, date: new Date(), content: 'hello world' }
+  ]);
+
   return (
     <main className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="NLW Expert" />
@@ -24,8 +24,10 @@ export default function App() {
 
       <section className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard />
-        <NoteCard note={note} />
-        <NoteCard note={note} />
+        
+        {notes.map(note => {
+          return <NoteCard note={note} />
+        })}
       </section>
     </main>
   )
